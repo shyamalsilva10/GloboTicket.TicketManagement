b2a513188a3e821927e53889f0521a89a344b6c5
Shyamal Anslem Silva <shyamal.silva@zho.gov.ae>
2025-12-30 16:06:14 +0400

    mapper and mediatr

---
+++ GloboTicket.TicketManagement.Application/Features/Events/GetEventListQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventListQueryHandler : IRequestHandler<GetEventListQuery, List<EventListVm>>
+    {
+        public readonly IAsyncRepository<Event> _eventRepository;
+        public readonly IMapper _mapper;
+        public GetEventListQueryHandler(IAsyncRepository<Event> eventRepository,IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<EventListVm>> Handle(GetEventListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _eventRepository.ListAllAsync()).OrderBy(x => x.Date);
+            return _mapper.Map<List<EventListVm>>(allEvents);
+            
+        }
+
+
+
+    }
+}
